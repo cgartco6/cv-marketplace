58771f478eed004e07bce7f62332451032a390a3
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-05-28 08:28:25 +0200

    Create RewriteCV.js

---
+++ frontend/src/pages/RewriteCV.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import CVUploader from '../components/CVUploader';
+export default function RewriteCV() { return (<div className="container py-8"><CVUploader /></div>); }
